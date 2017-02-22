a260e1b5a2499e3b468921050a08eb4eba129036
ebackers <arcestalavera@gmail.com>
2017-02-22 13:04:41 +0800

    update

--- application/views/modals/topic_description_modal.php
+++ application/views/modals/topic_description_modal.php
@@ -1,3 +1,6 @@
+<?php
+    $topic = $_SESSION['current_topic'];
+?>
 <!-- Topic Modal -->
 <div id="topic-modal" class="modal fade" role="dialog">
     <div class="modal-dialog">
@@ -5,11 +8,11 @@
         <div class="modal-content">
             <div class="modal-header modal-heading">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">TOPIC DESCRIPTION</h4>
+                <h4 class="modal-title text-center"><?php echo $topic->topic_name ?></h4>
             </div>
             <div class="modal-body">
                 <p>
-                    Asdfghjkl asdfghjkl asdfghjkl asdfhjklgasdfkljasdh laksdjg hasldkjfh alsdjkfh aslkdjgh alskdfj haslkdgj hawelkgj h Asdfghjkl asdfghjkl asdfghjkl asdfhjklgasdfkljasdh laksdjg hasldkjfh alsdjkfh aslkdjgh alskdfj haslkdgj hawelkgj h asdfhjklgasdfkljasdh laksd
+                    <?php echo $topic->topic_description ?>
                 </p>
                 <p>
                     Created by Juan Dela Cruz on Jan 2, 2017
