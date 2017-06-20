70cb583a62f07f2940c5c2b69b101ac649a78a59
atalavera <arcestalavera@gmail.com>
2017-06-20 14:30:12 +0800

    test

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -11,12 +11,12 @@
  *
  * @author Arces
  */
-class Search extends CI_Controller {
+class search extends CI_Controller {
 
     public function index() {
         $this->load->model("user_model", "users");
         $this->load->model("topic_model", "topics");
-        $keyword = htmlspecialchars($this->input->get("search-key"));
+        $keyword = htmlspecialchars($this->input->post("search-key"));
         $data['keyword'] = $keyword;
         $data['users'] = $this->users->search_users($keyword);
         $data['topics'] = $this->topics->search_topics($keyword);
