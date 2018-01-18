874c7d62a248a9d95e74ade8e1e8e67f5ad1ac7d
Aric Brillantes <aric_brillantes@dlsu.edu.ph>
2018-01-18 10:13:05 +0800

    Update (cookies work now)
    cookies work for all pages now
    (chosen colors are only temporary)

--- application/views/pages/search_page.php
+++ application/views/pages/search_page.php
@@ -1,6 +1,40 @@
 <?php
 include(APPPATH . 'views/header.php');
 ?>
+
+    <script>
+    
+        function getCookie(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+
+    //    function checkCookie() {
+    //        var user = getCookie("username");
+    //        if (user != "") {
+    //            alert("Welcome again " + user);
+    //        } else {
+    //            user = prompt("Please enter your name:", "");
+    //            if (user != "" && user != null) {
+    //                setCookie("username", user, 365);
+    //            }
+    //        }
+    //    }
+
+        document.write('<style type="text/css">body {background-color: ' + getCookie("backgroundColor") + ';}<\/style>');
+    
+    </script>
+
 <body>
     <?php
     include(APPPATH . 'views/navigation_bar.php');
