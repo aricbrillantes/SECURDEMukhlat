ee36ee78d548e16f46842dac6d96ce4e1eab2824
Jesus Castro <joesei_castro@dlsu.edu.ph>
2019-08-21 16:41:37 +0800

    added custom 404 page

---
+++ application/views/pages/error404.php
@@ -0,0 +1,18 @@
+<?php
+include(APPPATH . 'views/header.php');
+?>
+<script src="<?php echo base_url('zxcvbn-master/dist/zxcvbn.js'); ?>"></script>
+<script src="http://code.jquery.com/jquery-1.9.0.min.js"></script>
+<script>document.cookie = "timing=0;" + ";path=/";</script>
+
+<body class = "sign-in">
+    <div class = "container-fluid">
+        <text style="font-size:22px"><b>SECURDE MP</b><br>Brillantes, Aric<br>Chua, Liam<br>Libay, Iris</text>
+
+        <!-- Logo -->
+        <div class = "row sign-in-logo"><img src = "<?php echo base_url('images/logo/mukhlatlogo1.png'); ?>"></div>
+        <div class = "jumbotron text-center bg-none">
+            <h1> Error 404: Page not found. </h1>
+        </div>
+</body>
+</html>
